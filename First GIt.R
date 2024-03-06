aeae105a0fbeb55556f66c3348ec7ce3e3396a53
alyssalarson <alyssa.larson1022@gmail.com>
2024-03-06 11:55:28 -0600

    Lab

--- First GIt.R
+++ First GIt.R
@@ -1,2 +1,7 @@
 A <- c(2,4,5,6,9,5,1)
 mean(A)
+
+install.packages("usethis")
+usethis::use_git_config(user.name = "alyssalarson", user.email = "alyssa.larson1022@gmail.com")
+usethis::create_github_token() 
+credentials::set_github_pat("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")
